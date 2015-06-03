75e9f7ffaceb4b957b94e0fe4ef16c45f710ae9b
xSalice <zthanhz251@gmail.com>
2015-06-03 17:14:34 -0500

    x

--- xSaliceResurrected/xSaliceResurrected/Mid/Viktor.cs
+++ xSaliceResurrected/xSaliceResurrected/Mid/Viktor.cs
@@ -284,6 +284,7 @@ namespace xSaliceResurrected.Mid
                 foreach (
                     Obj_AI_Hero target in
                         HeroManager.Enemies
+                            .Where(x => x.IsValidTarget(3500) && !x.IsDead && x.Health > 0).OrderByDescending(x => x.Distance(_rObj.Position)))
                 {
                     Player.Spellbook.CastSpell(SpellSlot.R, target.ServerPosition);
                     _lastR = Utils.TickCount;
